7ad3a309a4f3ee98eb0cc480c93c5e7b0e9d8e5a
a99ressive <ulugbek99kzz@gmail.com>
2026-02-10 16:28:52 +0500

    Make Email Case Insensitive

---
+++ Migrations/20260210112523_MakeEmailCaseInsensitive.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeEmailCaseInsensitive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                DROP INDEX IF EXISTS "IX_Users_Email";
+
+                CREATE UNIQUE INDEX "IX_Users_Email_Lower"
+                ON "Users"(LOWER("Email"));
+            """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                DROP INDEX IF EXISTS "IX_Users_Email_Lower";
+
+                CREATE UNIQUE INDEX "IX_Users_Email"
+                ON "Users"("Email");
+            """);
+        }
+    }
+}
