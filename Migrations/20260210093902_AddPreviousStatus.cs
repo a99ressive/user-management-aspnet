b2c39403e1f09a6e53cc35b3ca7218147e89f5e5
a99ressive <ulugbek99kzz@gmail.com>
2026-02-10 14:42:56 +0500

    Previous Status Migration

---
+++ Migrations/20260210093902_AddPreviousStatus.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPreviousStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PreviousStatus",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PreviousStatus",
+                table: "Users");
+        }
+    }
+}
